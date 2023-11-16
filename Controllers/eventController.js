3d691f5e958fce1c0b1bc0e203ec12f44dc7743f
Mohamad Safa <mohsafa3@gmail.com>
2023-11-16 09:36:51 +0200

    Adding Success & message

--- Controllers/eventController.js
+++ Controllers/eventController.js
@@ -1,52 +1,51 @@
-const connection = require('../config/database');
+const connection = require("../config/database");
 
 exports.getEvents = async (req, res) => {
-    try {
-        const query1 = 'SELECT * FROM event';
-        const [result] = await connection.promise().query(query1);
-        res.status(200).json(result);
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({ message: "An error occured: couldn't get data " });
-    }
-}
+  try {
+    const query1 = "SELECT * FROM event";
+    const [result] = await connection.promise().query(query1);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "An error occured: couldn't get data " });
+  }
+};
 
 exports.addEvent = async (req, res) => {
-    try {
-        const {title,date,ticketPrice,description,venue_id} = req.body;
-        const query = `INSERT INTO event VALUES (${title},${date},${ticketPrice}),${description}),${venue_id})`;
-        const [result] = await connection.promise().query(query);
-        res.status(200).json(result);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "error occured : could not add data" });
-    }
-}
+  try {
+    const { title, date, ticketPrice, description, venue_id } = req.body;
+    const query = `INSERT INTO event VALUES (${title},${date},${ticketPrice}),${description}),${venue_id})`;
+    const [result] = await connection.promise().query(query);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error occured : could not add data" });
+  }
+};
 exports.updateEventByVenueId = async (req, res) => {
-    try {
-        const venue_id= req.params.name;
-        const event_id = req.params.id;
-        const query = `UPDATE enrollement SET present=NOT present WHERE venue_id=(SELECT user_id 
+  try {
+    const venue_id = req.params.name;
+    const event_id = req.params.id;
+    const query = `UPDATE enrollement SET present=NOT present WHERE venue_id=(SELECT user_id 
                      FROM users WHERE full_name='${venue_id}') AND event_id=${event_id}`;
-        const [result] = await connection.promise().query(query);
-        res.status(200).json(result);
-    } catch (error) {
-        console.log(error);
-        res.status(401).json({ message: "Could not update" })
-    }
-}
-
+    const [result] = await connection.promise().query(query);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "Could not update" });
+  }
+};
 
 exports.deleteEvent = async (req, res) => {
-    const venueId = req.params.id;
-    const eventId = req.params.ID;
-    try {
-        const query = `DELETE FROM event WHERE venue_id=${venueId} AND trainee_id=${eventId}`; 
-        const [result] = await connection.promise().query(query);
+  const venueId = req.params.id;
+  const eventId = req.params.ID;
+  try {
+    const query = `DELETE FROM event WHERE venue_id=${venueId} AND trainee_id=${eventId}`;
+    const [result] = await connection.promise().query(query);
 
-        res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'errorrr' });
-    }
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "errorrr" });
+  }
 };
--- Controllers/venueController.js
+++ Controllers/venueController.js
@@ -1,77 +1,85 @@
-const connection = require('../config/database');
+const { response } = require("express");
+const connection = require("../config/database");
 
 // HON AAM BAAML AAD
 exports.addVenue = async (req, res) => {
   try {
-    const {name,description,capacity,image,address} = req.body;
-    const query =`INSERT INTO venues (name,description,capacity,image,address) VALUES (${name},'${description}','${capacity}','${image}','${address}')`;
-    const [results]= await connection.promise().query(query);
+    const { name, description, capacity, image, address } = req.body;
+    const query = `INSERT INTO venues (name,description,capacity,image,address) VALUES (${name},'${description}','${capacity}','${image}','${address}')`;
+    const [results] = await connection.promise().query(query);
     res.status(201).json(results);
   } catch (error) {
-    console.error('Error adding venue:', error);
-    res.status(500).json({ error: 'Server Error'});
-  }
-}
-
-exports.getAllVenues = async (req, res) => {
-  try {
-    const query="SELECT * FROM venues";
-    const [result]=await connection.promise().query(query);
-    res.status(200).json(result);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({message:"ERRORRRR"});
+    console.error("Error adding venue:", error);
+    res.status(500).json({ error: "Server Error" });
   }
 };
 
-exports.getOneVenueById = async (req ,res) =>{
-  try{
-    const venueId=req.params.id;
-    const query=`SELECT * FROM venues WHERE venue_id= ${venueId}`;
-    const [result] = await connection.promise().query(query);
-    res.status(200).json(result);
+exports.getAllVenues = async (req, res) => {
+  try {
+    const query = "SELECT * FROM venues";
+    const [response] = await connection.promise().query(query);
+    res.status(200).json({
+      success: true,
+      message: `All venues retrieved successfully`,
+      data: response,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: `Unable to get all venues`,
+      error: error.message,
+    });
   }
-  catch(error){
-    console.log(error);
-    res.status(500).json({message:"ERRORRRR"});
-  }
-}
+};
 
-// HON AAM BAAML GET LA QUIZ WHDE BY COURSE_NAME
-exports.getOneVenueByVenueId = async (req ,res) =>{
-  try{
-    const venueId=req.params.id;
-    const query=`SELECT * FROM venues WHERE venue_id= ${venueId}`;
+// "".getAllVenues" (was done according to omar's correction session)
+//".getAdd" (how i did it)
+
+exports.getOneVenueById = async (req, res) => {
+  try {
+    const venueId = req.params.id;
+    const query = `SELECT * FROM venues WHERE venue_id= ${venueId}`;
     const [result] = await connection.promise().query(query);
     res.status(200).json(result);
-  }
-  catch(error){
+  } catch (error) {
     console.log(error);
-    res.status(500).json({message:"ERRORRRR"});
+    res.status(500).json({ message: "ERRORRRR" });
   }
-}
+};
+
+exports.getOneVenueByVenueId = async (req, res) => {
+  try {
+    const venueId = req.params.id;
+    const query = `SELECT * FROM venues WHERE venue_id= ${venueId}`;
+    const [result] = await connection.promise().query(query);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "ERRORRRR" });
+  }
+};
 
 exports.deleteVenue = async (req, res) => {
   const venueId = req.params.id;
   try {
-    const query=`DELETE FROM venues WHERE venue_id=${venueId}`;
-    const [result]= await connection.promise().query(query);
+    const query = `DELETE FROM venues WHERE venue_id=${venueId}`;
+    const [result] = await connection.promise().query(query);
     res.status(200).json(result);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'errorrr' });
+    res.status(500).json({ message: "errorrr" });
   }
 };
 
 exports.updateVenue = async (req, res) => {
   const venueId = req.params.id;
-  const {name,description,capacity,image,address}=req.body;
+  const { name, description, capacity, image, address } = req.body;
   try {
-    const query=`UPDATE venues SET name="${name}",description="${description},capacity="${capacity},image="${image},address="${address}" WHERE venue_id=${venueId}`;
-    const [result]= await connection.promise().query(query);
+    const query = `UPDATE venues SET name="${name}",description="${description},capacity="${capacity},image="${image},address="${address}" WHERE venue_id=${venueId}`;
+    const [result] = await connection.promise().query(query);
     res.status(200).json(result);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'errorrr' });
+    res.status(500).json({ message: "errorrr" });
   }
 };
